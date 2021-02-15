7697ef480d0420c876db3ccf4ee257391bfa9cc1
Dan Bartusek <76438714+bartuda@users.noreply.github.com>
2021-02-15 00:08:47 +0000

    Add files via upload

--- Flipside-API.py
+++ Flipside-API.py
@@ -13,21 +13,28 @@ print(response.text.encode('utf8'))
 
 import numpy as np
 import pprint
-
+import json
 
 r = requests.get("https://api.flipsidecrypto.com/api/v2/metrics/projects?api_key=3e2f65bf-7613-41f8-8e76-be8b7565c707")
 r.json()
-pprint.pprint(r.json())                                      #["posts"] - create a list of posts
+#pprint.pprint(r.json())                                      #["posts"] - create a list of posts
+
 #pprint.pprint(r.json()["posts"][0])                         #[0] - index of in posts
 #pprint.pprint(r.json()["posts"][0]["url_for_post"])         #request only the url
 
+r_text = r.text
+
+data = json.loads(r_text)
+print(data)
+print(type(data))                       #dictionary
+
+data_serialised = json.dumps(data)
+print(type(data_serialised))            #string
+
+data_serialised = json.dump(data, open('data.json', "w"), indent = 4)
 
 
-#RESULT
-#[{'cmc_id': 2569,
-#          'id': '0031aea9-2b0d-4052-9a80-4efd28271e45',
-#          'name': 'CoinPoker',
-#          'short_name': 'CoinPoker',
-#          'slug': '',
-#          'symbol': 'CHP'},
+
+
+
 
